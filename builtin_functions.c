711c33ed8fe138abe3379ee65b6b871b61ef2401
AlWaleed Musa <jako_249@hotmail.com>
2023-12-12 20:31:05 +0200

    added a find buildin function + string memcpy function

--- builtin_functions.c
+++ builtin_functions.c
@@ -14,3 +14,34 @@ int exit_func(char *commands_array[], char *argv)
 
 	exit(0);
 }
+
+/**
+ * find_builtin - Searches an array of FuncInfo structs to find the
+ * function pointer for the given command name.
+ * @name: The name of the command to find.
+ * @funcs: Array of FuncInfo structs containing name and function pointer.
+ * @num_funcs: Number of elements in funcs array.
+ *
+ * Return: a function pointer if found, NULL if not found.
+ */
+FuncPtr find_builtin(char *name, struct FuncInfo *funcs, int num_funcs)
+{
+	int i = 0;
+	char *token = strtok(name, " \n");
+
+	if (token == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < num_funcs)
+	{
+		if (_strcmp(token, funcs[i].name) == 0)
+		{
+			free(token);
+			return (funcs[i].func);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- string_functions.c
+++ string_functions.c
@@ -18,3 +18,22 @@ int _strlen(char *s)
 
 	return (longi);
 }
+
+/**
+ * _memcpy - function to copy memory
+ * @src: source to copy from
+ * @dest: destination to copy to
+ * @n: num of chars to copy
+ *
+ * Return: Copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int lol;
+
+	for (lol = 0; lol < n; lol++)
+	{
+		dest[lol] = src[lol];
+	}
+	return (dest);
+}
