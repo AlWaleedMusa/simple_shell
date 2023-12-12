2f94472e95f6812af284e48579686ec0e871c64d
AlaaElrasheed <wethag.234@gmail.com>
2023-12-12 11:36:13 -0800

    5

--- split.c
+++ split.c
@@ -26,3 +26,52 @@ int count_word(char *s)
 
 	return (g);
 }
+
+/**
+ * **strtow - Entry point
+ *
+ * @str: string
+ *
+ * Return: pointer to an array (Success)
+ */
+
+char **strtow(char *str)
+{
+	char **lmao, *lol;
+	int w, q = 0, length = 0, words, s = 0, start, end;
+
+	while (*(str + length))
+		length++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	lmao = (char **)malloc(sizeof(char *) * (words + 1));
+	if (lmao == NULL)
+		return (NULL);
+
+	for (w = 0; w <= length; w++)
+	{
+		if (str[w] == ' ' || str[w] == '\0')
+		{
+			if (s)
+			{
+				end = w;
+				lol = (char *)malloc(sizeof(char) * (s + 1));
+				if (lol == NULL)
+					return (NULL);
+				while (start < end)
+					*lol++ = str[start++];
+				*lol = '\0';
+				lmao[q] = lol - s;
+				q++;
+				s = 0;
+			}
+		}
+		else if (s++ == 0)
+			start = w;
+	}
+	lmao[q] = NULL;
+
+	return (lmao);
+}
