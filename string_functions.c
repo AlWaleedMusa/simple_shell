3eff4e2af9de90371225e83def1c374e5a51f60d
AlaaElrasheed <wethag.234@gmail.com>
2023-12-12 11:21:23 -0800

    1

--- string_functions.c
+++ string_functions.c
@@ -37,3 +37,27 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }
+
+/**
+ * _strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
