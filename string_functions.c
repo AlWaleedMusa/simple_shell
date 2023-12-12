3f6f2a5072267549d3932fab4e163d4497a1ff7d
AlaaElrasheed <wethag.234@gmail.com>
2023-12-12 11:24:51 -0800

    3

--- string_functions.c
+++ string_functions.c
@@ -83,3 +83,22 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+
+/**
+ * _strchr - Entrey point
+ * @s: parameter deal with the function
+ * @c: parameter deal with function
+ *
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int lmao;
+
+	for (lmao = 0; s[lmao] >= '\0'; lmao++)
+	{
+		if (s[lmao] == c)
+			return (s + lmao);
+	}
+	return (0);
+}
