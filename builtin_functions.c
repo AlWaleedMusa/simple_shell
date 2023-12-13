2d5782b213b023862d0d423e9c68d7d4d7b19584
AlaaElrasheed <wethag.234@gmail.com>
2023-12-13 09:55:23 -0800

    l

---
+++ builtin_functions.c
@@ -0,0 +1,137 @@
+#include "main.h"
+
+/**
+ * exit_func - Exits the shell with exit code 0.
+ * @commands_array: Array of command line arguments.
+ * @argv: argument to the program.
+ *
+ * Return: nothing. Exits the program with exit code 0.
+ */
+int exit_func(char *commands_array[], char *argv)
+{
+	(void)commands_array;
+	(void)argv;
+
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * env_func - Prints the current environment variables to stdout.
+ * @commands_array: Array of command line arguments.
+ * @argv: argument to the program.
+ *
+ * Return: 0 on success.
+ */
+int env_func(char *commands_array[], char *argv)
+{
+	int i = 0;
+
+	(void)commands_array;
+	(void)argv;
+
+	while (environ[i])
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+	exit(0);
+}
+
+/**
+ *  cd_func - Changes the current working directory.
+ * @commands_array: Array of command line arguments.
+ * @argv: argument to the program.
+ *
+ * Return: Returns 0 on success, non-zero on failure.
+ */
+int cd_func(char *commands_array[], char *argv)
+{
+	char curr_cwd[1024];
+	int status = -1;
+
+	(void)argv;
+
+	if (commands_array[1] == NULL)
+	{
+		status = chdir(my_getenv("HOME"));
+	}
+	else if (_strcmp(commands_array[1], "-") == 0)
+	{
+		status = chdir(my_getenv("OLDPWD"));
+	}
+	else
+	{
+		status = chdir(commands_array[1]);
+	}
+
+	if (status == -1)
+	{
+		perror("Error");
+		return (-1);
+	}
+	else
+	{
+		getcwd(curr_cwd, sizeof(curr_cwd));
+		setenv("OLDPWD", my_getenv("PWD"), 1);
+		setenv("PWD", curr_cwd, 1);
+	}
+	return (0);
+}
+
+/**
+ * find_builtin - Searches an array of FuncInfo structs to find the
+ * function pointer for the given command name.
+ * @name: The name of the command to find.
+ * @funcs: Array of FuncInfo structs containing name and function pointer.
+ * @num_funcs: Number of elements in funcs array.
+ *
+ * Return: a function pointer if found, NULL if not found.
+ */
+FuncPtr find_builtin(char *name, struct FuncInfo *funcs, int num_funcs)
+{
+	int i = 0;
+	char *token = strtok(name, " \n");
+
+	if (token == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < num_funcs)
+	{
+		if (_strcmp(token, funcs[i].name) == 0)
+		{
+			free(token);
+			return (funcs[i].func);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * builtin - Checks if the given command is a builtin, and if so calls
+ * the corresponding function.
+ * @commands_array: Array of command and arguments.
+ * @argv: argument to the program.
+ *
+ * Return: 0 if a builtin function was found and executed, -1 otherwise.
+ */
+int builtin(char *commands_array[], char *argv)
+{
+	funcinfo funcs[] = {
+		{"exit", exit_func},
+		{"env", env_func},
+		{"cd", cd_func}};
+
+	int num_funcs = sizeof(funcs) / sizeof(struct FuncInfo);
+	FuncPtr func = find_builtin(commands_array[0], funcs, num_funcs);
+
+	if (func != NULL)
+	{
+		func(commands_array, argv);
+		return (0);
+	}
+	return (-1);
+}
