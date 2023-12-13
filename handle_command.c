b79540395e4e73837fa62ba233548252e0f6664a
AlaaElrasheed <wethag.234@gmail.com>
2023-12-13 04:53:40 -0800

    k

--- handle_command.c
+++ handle_command.c
@@ -15,13 +15,7 @@ int handle_command(char *commands_array[], char *argv)
 	{
 		return (-1);
 	}
-
-	result = builtin(commands_array, argv);
-	if (result == -2)
-	{
-		return (-2);
-	}
-	else if (result != 0)
+	if (builtin(commands_array, argv) != 0)
 	{
 		execute_cmd(commands_array, argv);
 	}
